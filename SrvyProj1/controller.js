be8205bbcf96693e004824498d31a5a5c958b671
jacobvelasco <jacobvelasco91@gmail.com>
2017-10-25 23:34:07 -0700

    finished object constructor created to make questions

--- SrvyProj1/controller.js
+++
@@ -1,6 +0,0 @@
-function Controller () {
-
-}
-Controller.prototype.getUrl(){
-  
-}
---
+++ SrvyProj1/js/controller.js
@@ -0,0 +1,7 @@
+function Controller () {
+
+}
+Controller.prototype.geturl = function (url) {
+  var info = url.split("&");
+  var nameVpair = info[1].split("&");
+}
