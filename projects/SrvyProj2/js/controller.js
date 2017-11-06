6b5b0619c8dadb6fe970b72c790222f8af59a9f5
jacobvelasco <jacobvelasco91@gmail.com>
2017-11-06 11:39:19 -0800

    adding second week of projects folder

---
+++ projects/SrvyProj2/js/controller.js
@@ -0,0 +1,6 @@
+function create(){
+var element = document.createElement('form');
+var content = document.createElement('input');
+element.appendChild(content);
+document.body.appendChild(element);
+}
---
+++ projects/SrvyProj2/js/survey.js
@@ -0,0 +1,14 @@
+/*Build a Survey function constructor (class) that Will recieve a question
+object.*/
+
+function Survey(Obj){
+   //take in an array of question objects
+      // - this will have the answers in them
+   //take in a title
+   //take in a description of the survey
+}
+
+//METHODS of the Survey constructor
+//Display the title
+//display the description
+//display the questions with their answers and name(key) attribute;
