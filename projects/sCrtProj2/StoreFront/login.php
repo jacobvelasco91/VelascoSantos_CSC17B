34f96be5dd9bab0f6412921cce8bea22d09cb877
jacobvelasco <jacobvelasco91@gmail.com>
2017-11-12 22:45:07 -0800

    created signup form

--- projects/sCrtProj2/StoreFront/login.php
+++ projects/sCrtProj2/StoreFront/login.php
@@ -1,17 +1,3 @@
-    <?php
-    $validacc = "jacobvelasco91@gmail.com";
-    if (isset($_POST['submit'])) {
-      $email1 = $_POST["email"];
-      if ($email1 != $validacc) {
-        function noacc(){
-          echo "<p class='noacc'>No account found</p>";
-        }
-      }else {
-        header('Location:./index.html');
-      }
-    }
-
-    ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -22,6 +8,22 @@
     <title>AllThings|Login</title>
   </head>
   <body>
+    <?php
+    //check if there is a request made to the server | if yes, enter statement
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+      //Get information from the login form | already validated in JS
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+      //make connection to database
+      $dbc = @mysqli_connect("localhost","root","","Newsletter");
+      //make query | check database for email and password account
+      $query = "";
+      //execute query | if there is a account with email and password
+      if (@mysqli_query($dbc,$query)) {
+
+      }
+    }
+     ?>
 
     <!--Header Area-->
     <header>
@@ -35,7 +37,6 @@
     <form class="loginform" name="loginform" action="login.php" method="post" onsubmit="return validate()">
         <img src="./images/logo.png" alt="logo"><br>
         <h1>sign in</h1><br><br>
-        <?php if (isset($_POST['submit'])) { noacc(); } ?>
         <fieldset>
           <legend id="legendemail">email</legend>
           <input type="text" name="email" placeholder="..." required>
--- projects/sCrtProj2/StoreFront/signup.php
+++ projects/sCrtProj2/StoreFront/signup.php
@@ -1,10 +1,52 @@
+<!--When creating an account, it would be good to check if an email has already
+been used. If it has, return error to user, if its okay, continue with account creation-->
+
+
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>register account</title>
+    <title>All Things | register account</title>
   </head>
   <body>
+
+    <?php
+
+     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+       //connect to database first
+       $dbc = new mysqli("localhost","root","","Newsletter");
+       //set charset code
+       $dbc->set_charset('utf-8');
+       //grab info from form email,firstname,lastname,password and strip
+       $signup_email = $dbc->real_escape_string(trim(strip_tags($_POST['email'])));
+       $signup_fname = $dbc->real_escape_string(trim(strip_tags($_POST['fname'])));
+       $signup_lname = $dbc->real_escape_string(trim(strip_tags($_POST['lname'])));
+       $signup_password = $dbc->real_escape_string(trim(strip_tags($_POST['password'])));
+       //Make a query to insert into table
+       $query = "INSERT INTO accounts (email,fname,lname,password) VALUES('$signup_email','$signup_fname','$signup_lname','$signup_password')";
+       //execute query
+       if ($dbc->query($query)) {
+         echo "account created";
+       }else{
+         echo "There was a problem creating an account";
+       }
+      }
+       ?>
+
+
+<!--HEADER-->
+      <p>Register Account</p>
+      <hr>
+<!--FORM-->
+     <form class="" action="signup.php" method="post">
+       <p>EMAIL:<input type="text" name="email"></p>
+       <p>First Name:<input type="text" name="fname"></p>
+       <p>Last Name:<input type="text" name="lname"></p>
+       <p>Password:<input type="password" name="password"></p>
+       <input type="submit" name="submit">
+     </form>
+
 
   </body>
 </html>
